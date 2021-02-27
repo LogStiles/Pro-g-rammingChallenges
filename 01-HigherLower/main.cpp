e72bbfa0c46676e649c995a110931b4b678f0aed
StileDev <lstiles507@gmail.com>
2021-02-26 21:16:59 -0500

    completed challenge 01

---
+++ 01-HigherLower/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**
+ * A game of Higher/Lower
+ */
+int main() {
+    int goal;
+    int guess;
+    string userInput;
+
+    while(true) {
+        printf("Let's play a game of Higher/Lower.\n");
+        printf("I'm thinking of a number between 1 and 100.\n");
+        goal = rand() % 100 + 1;
+        guess = 100;
+        while (goal != guess) {
+            printf("What's your guess? ");
+            getline(cin, userInput);
+
+            try { //check if input is valid
+                guess = stoi(userInput);
+            } catch(const invalid_argument& ia) {
+                printf("Invalid input! Please enter an integer.\n");
+                continue;
+            }
+
+            if (goal > guess) {
+                printf("Higher!\n");
+            } else if (goal < guess) {
+                printf("Lower!\n");
+            }
+        }
+
+        printf("You got it!\n");
+        printf("Would you like to play again?: ");
+        getline(cin, userInput); 
+        if (userInput[0] == 'n' or userInput[1] == 'N') { //if the answer starts with 'N' we'll assume the answer was "No"
+            break;
+        }
+    }
+    printf("Bye!");
+    return 0;
+}
